6aa45bdb623de9dd5f0e5e27b3e9cd6ba2e583f9
Julian Brown <brownjulian2@gmail.com>
2015-02-06 18:48:58 -0500

    refactor wm crawler

--- api/wm.js
+++ api/wm.js
@@ -1,34 +1,33 @@
 var secret = require('../../secret');
-var data = '';
 var request = require('request');
-var save = require('./save');
+var mergeData   = require('./mergeData');
 var url = '';
 var key = secret.wmApiKey;
-var storeName = 'Walmart',
-    storeID   = 'wm';
+var STORE_NAME = 'Walmart',
+    STORE_ID   = 'wm';
 
 
-exports.crawl = function(formData){
+exports.crawl = function(formData, callback){
   url = 'http://walmartlabs.api.mashery.com/v1/items/'+formData.id+'?format=json&apiKey='+key;
   request(url, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-
-      data            = JSON.parse(body); // Print the google web page.
-
-      data.id         = formData.id;
-      data.myPrice    = formData.myPrice;
+      var data;
+      data            = JSON.parse(body); 
+      data.stock      = data.availableOnline;
+      data.storeName  = STORE_NAME;
+      data.storeID    = STORE_ID;
       data.storePrice = data.salePrice || 'Not Available';
       data.image      = data.thumbnailImage;
       data.stock      = data.availableOnline;
       data.link       = data.productUrl;
-      data.storeName  = storeName;
-      data.storeID    = storeID;
-      data.upperLimit = formData.upperLimit;
-      data.lowerLimit = formData.lowerLimit;
-      data.status     = formData.status;
       data.title      = data.name || 'Not Available';
 
-      save(data);
+
+      // merge crawl data and form data
+      mergeData.init( data, formData, function( data ) {
+        callback(data);
+      });
+
     } else {
       console.log('Walmart request failed');
     }
